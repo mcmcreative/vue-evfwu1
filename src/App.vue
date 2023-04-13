e71f493bf1dd70e95e90fbde5bab5f611d02e10e
mcmcreative <63327024+mcmcreative@users.noreply.github.com>
2023-04-13 11:53:39 -0400

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="https://th.bing.com/th/id/R.76b3145515e1884e3a041c2b8129b1b5?rik=0p%2bOIRuSjYg5uw&riu=http%3a%2f%2fitc-bolivia.com%2fwp-content%2fuploads%2f2019%2f05%2fBanco-Bisa.png&ehk=%2bzecoIz8KnbwJT96meFgbnM4Mb05rQOFm5b3z35coKI%3d&risl=&pid=ImgRaw&r=0" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Bienvenido a Banco Bisa" />
   </div>
 </template>
 
